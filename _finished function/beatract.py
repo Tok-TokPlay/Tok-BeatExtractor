dab21266f810b6262560cdf8e1878ae84225fe50
최현준 <chj878194@naver.com>
2017-05-24 14:30:25 +0900

    indentation modified
    indentation modified with tap( sapce 4 ) from 4 sapce

--- _finished function/beatract.py
+++ _finished function/beatract.py
@@ -44,97 +44,96 @@ def take_local_maximum(CQT_result, threshold) :
     Raises : 
         nothing.
     '''
-    high = []
-    result = []
-    for t in range(0, len(CQT_result[0])) :
-        for i in range(0, len(CQT_result)) :
-            if CQT_result[i][t] > threshold :
-                # i th scale at time t is larger then threshold...
-                high.append(i)
+	high = []
+	result = []
+	for t in range(0, len(CQT_result[0])) :
+		for i in range(0, len(CQT_result)) :
+			if CQT_result[i][t] > threshold :
+				# i th scale at time t is larger then threshold...
+				high.append(i)
 				# pick up.
-        result.append(high)
-        high = []
+		result.append(high)
+		high = []
 		# Add list to result and initialize high to empty list.
-    return result
+	return result
 
 
 def get_threshold(CQT_result, seed = 0.75, result_hop = 1000) :
-    '''
-    Return the threshold number for CQT_result with differential value.
-    Args : CQT_result, seed, result_hop
-   		CQT_result - the list of CQT's output.
-        seed - the seed of how far from standard value to mim / max values. default is 0.75 and this value should be real value at 0.5 ~ 1.
+	'''
+	Return the threshold number for CQT_result with differential value.
+	Args : CQT_result, seed, result_hop
+		CQT_result - the list of CQT's output.
+		seed - the seed of how far from standard value to mim / max values. default is 0.75 and this value should be real value at 0.5 ~ 1.
 		result_hop - ignore value to take tangent of values. default is 1000
-    Returns : 
+	Returns : 
 		min / max value of threshold. threshold value.
-    Raises : 
-        nothing.
-    '''
-    result_list = []
-    for f in range(0, len(CQT_result)) :
-        for t in range(0, len(CQT_result[0])) :
-            result_list.append(abs(CQT_result[f][t]))
-	
-    result_list.sort()
-    # Copy List to sort result.
-    sorted_tangent = []
-    # to using Differential, get tangent values.
-    for i in range(0, len(result_list) - result_hop) :
-        sorted_tangent.append((result_list[i + result_hop] - result_list[i]) / result_hop)
-        maximum_tangent = 0
-        maximum_index = -1
-        for i in range(0, len(sorted_tangent)) :
-            if maximum_tangent < sorted_tangent[i] :
-                maximum_tangent = sorted_tangent[i]
-                maxumum_index = i
-		
+	Raises : 
+		nothing.
+	'''
+	result_list = []
+	for f in range(0, len(CQT_result)) :
+		for t in range(0, len(CQT_result[0])) :
+			result_list.append(abs(CQT_result[f][t]))
+
+	result_list.sort()
+	# Copy List to sort result.
+	sorted_tangent = []
+	# to using Differential, get tangent values.
+	for i in range(0, len(result_list) - result_hop) :
+		sorted_tangent.append((result_list[i + result_hop] - result_list[i]) / result_hop)
+		maximum_tangent = 0
+		maximum_index = -1
+		for i in range(0, len(sorted_tangent)) :
+			if maximum_tangent < sorted_tangent[i] :
+				maximum_tangent = sorted_tangent[i]
+				maxumum_index = i
+
 	small_tangent_index = -1
 	large_tangent_index = -1
 	# get result with calcuated tangent values.
 	for i in range(0, len(sorted_tangent)) :
- 		if small_tangent_index == -1 :
+		if small_tangent_index == -1 :
 			if sorted_tangent[i] > (1 - seed) * maximum_tangent :
 				small_tangent_index = i
 		if large_tangent_index == -1 :
 			if sorted_tangent[i] > (seed) * maximum_tangent :
 				large_tangent_index = i
 
-    return result_list[small_tangent_index], result_list[large_tangent_index]
+	return result_list[small_tangent_index], result_list[large_tangent_index]
 
 def parse_noise(CQT_result, MAG_threshold) :
-    '''
-    parsing noise of CQT_result. result will be real harmonic sound which is bigger then threshold.
-    Args : CQT_result, MAG_threshold
+	'''
+	parsing noise of CQT_result. result will be real harmonic sound which is bigger then threshold.
+	Args : CQT_result, MAG_threshold
 		CQT_result - Mixture of CQT_result that big sound and small sound.
 		MAG_threshold - Standard for judge big and small.
-    Returns : CQT_noise, CQT_harmonic
+	Returns : CQT_noise, CQT_harmonic
 		CQT_noise - small sound which judged to noise sound.
 		CQT_harmonic - big sound which judged to big sound.
-    Raises : 
-        nothing.
+	Raises : 
+		nothing.
     '''
 	# Make empty 2 by 2 list.
 	# Dimension of output list is same as CQT_result (input list).
-    CQT_noise = []
-    CQT_harmonic = []
-    for f in range(0, len(CQT_result)) :
-        CQT_noise.append([])
-        CQT_harmonic.append([])
+	CQT_noise = []
+	CQT_harmonic = []
+	for f in range(0, len(CQT_result)) :
+		CQT_noise.append([])
+		CQT_harmonic.append([])
 	
-    for f in range(0, len(CQT_result)) :
+	for f in range(0, len(CQT_result)) :
 	# f is frequency ( Note, CQT values ).
-        for t in range(0, len(CQT_result[0])) :
+		for t in range(0, len(CQT_result[0])) :
 		# t is time.
-            if abs(CQT_result[f][t]) > MAG_threshold :
+			if abs(CQT_result[f][t]) > MAG_threshold :
 			# if bigger then MAG_threshold...
-                CQT_harmonic[f].append(CQT_result[f][t])
-                CQT_noise[f].append(0)
-            else :
+				CQT_harmonic[f].append(CQT_result[f][t])
+				CQT_noise[f].append(0)
+			else :
 			# if smaller then MAG_threshold
-                CQT_harmonic[f].append(0)
-                CQT_noise[f].append(CQT_result[f][t])
-	
-    return CQT_noise, CQT_harmonic
+				CQT_harmonic[f].append(0)
+				CQT_noise[f].append(CQT_result[f][t])
+	return CQT_noise, CQT_harmonic
 
 def get_scale(CQT_harmonic) :
 	'''
@@ -148,34 +147,34 @@ def get_scale(CQT_harmonic) :
 	Raises : 
 		Nothing
 	'''
-    time_set = []
-    scale_set = []
+	time_set = []
+	scale_set = []
 
-    for f in range(0, len(CQT_harmonic)) :
-        scale_set.append([])
-        time_set.append([])
+	for f in range(0, len(CQT_harmonic)) :
+		scale_set.append([])
+		time_set.append([])
 
-    recording = False
-    st = 0
-    ft = 0
-    temp = []
-    for f in range(0, len(CQT_harmonic)) :
-        for t in range(0, len(CQT_harmonic[0])) :
-            if recording == True :
-                if abs(CQT_harmonic[f][t]) == 0 :
-                    ft = t - 1
-                    scale_set[f].append(temp)
-                    time_set[f].append([st,ft])
-                    recording = False
-                    temp = []
-                else :
-                    temp.append(CQT_harmonic[f][t])
-            else :
-                if abs(CQT_harmonic[f][t]) != 0 :
-                    st = t
-                    recording = True
-                    temp.append(CQT_harmonic[f][t])
-    return scale_set, time_set
+	recording = False
+	st = 0
+	ft = 0
+	temp = []
+	for f in range(0, len(CQT_harmonic)) :
+		for t in range(0, len(CQT_harmonic[0])) :
+			if recording == True :
+				if abs(CQT_harmonic[f][t]) == 0 :
+					ft = t - 1
+					scale_set[f].append(temp)
+					time_set[f].append([st,ft])
+					recording = False
+					temp = []
+				else :
+					temp.append(CQT_harmonic[f][t])
+			else :
+				if abs(CQT_harmonic[f][t]) != 0 :
+					st = t
+					recording = True
+					temp.append(CQT_harmonic[f][t])
+	return scale_set, time_set
 
 def get_scale_simmilarity(scale_set) :
 	'''
@@ -187,19 +186,19 @@ def get_scale_simmilarity(scale_set) :
 	Raises :
 		nothing
 	'''
-    scale_list = []
-    DTW_value = []
-    for f in range(0, len(scale_set)) :
-        for t in range(0, len(scale_set[f])) :
-            scale_list.append(scale_set[f][t])
-            DTW_value.append([])
-    for i in range(0, len(scale_list)) :
-        for j in range(0, len(scale_list)) :
-            if j < i :
-                DTW_value[i].append(DTW_value[j][i])
-            else : 
-                DTW_value[i].append(MCC_with_DTW(scale_list[i], scale_list[j]))    
-    return DTW_value
+	scale_list = []
+	DTW_value = []
+	for f in range(0, len(scale_set)) :
+		for t in range(0, len(scale_set[f])) :
+			scale_list.append(scale_set[f][t])
+			DTW_value.append([])
+	for i in range(0, len(scale_list)) :
+		for j in range(0, len(scale_list)) :
+			if j < i :
+				DTW_value[i].append(DTW_value[j][i])
+			else : 
+				DTW_value[i].append(MCC_with_DTW(scale_list[i], scale_list[j]))
+	return DTW_value
 
 def make_empty_list(a = -1, b = -1, c = -1, d = -1) :
 	'''
@@ -211,34 +210,34 @@ def make_empty_list(a = -1, b = -1, c = -1, d = -1) :
 	Raises :
 		nothing
 	'''
-    result_list = []
-    # One Dimension list.
-    if a != -1 and b == -1:
-        result_list = []
-    # Two Dimension list.
-    elif b != -1 and c == -1 :
-        for i1 in range(0, b) :
-            result_list.append([])
-    # Three Dimension list.
-    elif c != -1 and d == -1 :
-        for i1 in range(0, b) :
-            result_list.append([])
-        for i1 in range(0, b) :
-            for i2 in range(0, c) :
-                result_list[i1].append([])
-    # Four Dimension list.
-    else :
-        for i1 in range(0, b) :
-            result_list.append([])
-        for i1 in range(0, b) :
-            for i2 in range(0, c) :
-                result_list[i1].append([])
-        for i1 in range(0, b) :
-            for i2 in range(0, c) :
-                for i3 in range(0, d) :
-                    result_list[i1][i2].append([])
+	result_list = []
+	# One Dimension list.
+	if a != -1 and b == -1:
+		result_list = []
+	# Two Dimension list.
+	elif b != -1 and c == -1 :
+		for i1 in range(0, b) :
+			result_list.append([])
+	# Three Dimension list.
+	elif c != -1 and d == -1 :
+		for i1 in range(0, b) :
+			result_list.append([])
+		for i1 in range(0, b) :
+			for i2 in range(0, c) :
+				result_list[i1].append([])
+	# Four Dimension list.
+	else :
+		for i1 in range(0, b) :
+			result_list.append([])
+		for i1 in range(0, b) :
+			for i2 in range(0, c) :
+				result_list[i1].append([])
+		for i1 in range(0, b) :
+			for i2 in range(0, c) :
+				for i3 in range(0, d) :
+					result_list[i1][i2].append([])
 	# Can make it with recursive function with dimension list.
-    return result_list
+	return result_list
 
 def make_empty_list_recursion(dimension, to_make)	:
 	'''
@@ -261,3 +260,14 @@ def make_empty_list_recursion(dimension, to_make)	:
 			to_make.append([])
 		return to_make	
 		# if len(dimension) is one, then stop recursion and return to_make list so can process other part of functions.
+
+def stage_note(r_harmonics) : 
+	'''
+	stage notes to make decision which notes are same instrumental.
+	Args : 
+		
+	Returns :
+		
+	Raises : 
+		nothing
+	'''
