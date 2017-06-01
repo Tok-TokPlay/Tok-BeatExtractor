91017358c55da5173e2ab64a0d704ec12dc68629
최현준 <chj878194@naver.com>
2017-06-01 13:12:35 +0900

    setup.py created
    setup.py created

---
+++ _finished function/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+ 
+setup(
+    name = "bitnang",
+    version = "100",
+    author = "bitnang",
+    author_email = "bitnang@naver.com",
+    py_modules=['bitnang'],
+    url = "blog.naver.com/bitnang",
+    description = "bitnang",
+)
