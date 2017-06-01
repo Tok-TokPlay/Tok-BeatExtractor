0855de7ec3e298eb1fa2aa770cb96a3c41aaf2d9
최현준 <chj878194@naver.com>
2017-06-01 19:42:06 +0900

    v1.2 release
    v1.2 release

---
+++ beatract-1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "beatract",
+    version = "1.2",
+    author = "Hyun Joon Choi",
+    author_email = "chj878194@naver.com",
+    packages=['beatract'],
+    py_modules=['beat_cqt', 'beat_tie'],
+    url = "https://github.com/Tok-TokPlay/Tok-BeatExtractor"
+)
